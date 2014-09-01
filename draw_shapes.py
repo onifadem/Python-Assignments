fa13b1ae3e71d34efab1a15fbb90e15fb920e4af
maryam <maryam@aims.edu.gh>
2014-09-01 11:57:50 +0000

    drawing shapes

---
+++ draw_shapes.py
@@ -0,0 +1,34 @@
+__author__ = 'maryam'
+"""import turtle
+def draw_square(side):
+    i=0
+    while(i<4):
+        turtle.forward(side)
+        turtle.left(90)
+        i=i+1
+    turtle.done()
+
+draw_square(50)"""
+
+"""import turtle
+def draw_rectangle(lenght, breadth):
+    i=0
+    while(i<2):
+        turtle.forward(lenght)
+        turtle.left(90)
+        turtle.forward(breadth)
+        turtle.left(90)
+        i=i+1
+    turtle.done()
+draw_rectangle(100,50)"""
+
+import turtle
+def draw_circle(diameter):
+    i=0
+    while(i<2):
+        turtle.forward(diameter)
+        turtle.left(360)
+        i=i+1
+    turtle.done()
+draw_circle(90)
+
